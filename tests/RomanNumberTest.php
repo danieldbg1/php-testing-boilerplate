2687e61082c0f394f9cd1b9a2e9c5b7b6c2e3b1f
Daniel del Barrio <danieldbg1@gmail.com>
2021-02-25 14:52:36 +0100

    Renombrado de variables en el RomanNumberTest.php

--- tests/RomanNumberTest.php
+++ tests/RomanNumberTest.php
@@ -11,11 +11,11 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_1_devuelve_I(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(1);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(1);
 
-        $this->assertEquals("I", $resultado);
+        $this->assertEquals("I", $romanNumber);
 
     }
 
@@ -23,22 +23,22 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_5_devuelve_V(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(5);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(5);
 
-        $this->assertEquals("V", $resultado);
+        $this->assertEquals("V", $romanNumber);
     }
 
     /**
      * @test
      */
     public function si_es_10_devuelve_X(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(10);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(10);
 
-        $this->assertEquals("X", $resultado);
+        $this->assertEquals("X", $romanNumber);
 
     }
 
@@ -46,9 +46,9 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_50_devuelve_L(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(50);
+        $resultado = $decimalNumber->convertirNumerosRomanos(50);
 
         $this->assertEquals("L", $resultado);
 
@@ -58,11 +58,11 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_100_devuelve_C(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(100);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(100);
 
-        $this->assertEquals("C", $resultado);
+        $this->assertEquals("C", $romanNumber);
 
     }
 
@@ -70,11 +70,11 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_500_devuelve_D(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(500);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(500);
 
-        $this->assertEquals("D", $resultado);
+        $this->assertEquals("D", $romanNumber);
 
     }
 
@@ -82,11 +82,11 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function si_es_1000_devuelve_M(){
-        $romanNumber = new RomanNumber();
+        $decimalNumber = new RomanNumber();
 
-        $resultado = $romanNumber->convertirNumerosRomanos(1000);
+        $romanNumber = $decimalNumber->convertirNumerosRomanos(1000);
 
-        $this->assertEquals("M", $resultado);
+        $this->assertEquals("M", $romanNumber);
 
     }
 
@@ -94,17 +94,17 @@ class RomanNumberTest extends TestCase{
      * @test
      */
     public function devuelve_decimal_minimo_maximo(){
-        $romanNumber = new RomanNumber();
+        $decimalnumber = new RomanNumber();
 
-        $this->assertEquals(1, $romanNumber->buscarNumeroMinimoMaximo(3));
-        $this->assertEquals(50, $romanNumber->buscarNumeroMinimoMaximo(88));
-        $this->assertEquals(10, $romanNumber->buscarNumeroMinimoMaximo(12));
+        $this->assertEquals(1, $decimalnumber->buscarNumeroMinimoMaximo(3));
+        $this->assertEquals(50, $decimalnumber->buscarNumeroMinimoMaximo(88));
+        $this->assertEquals(10, $decimalnumber->buscarNumeroMinimoMaximo(12));
     }
 
     /**
      * @test
      */
-    public function devuelve(){
+    public function devuelve_numero_descompuesto_en_romano(){
         //comprobar si metiendo un numero, devuelve su descomposicion en romano, p.e.:
         //meto 3, devuelve III, meto 18, devuelve XVIII.Sin comprobar los 4, 9, 90,...
     }
